fee28c13524ee798943e6ccacb7bef238832ae73
AstaFrode <frode@cess.one>
2023-08-10 15:05:45 +0800

    update pois-service-api.proto

--- core/poisService.go
+++ core/poisService.go
@@ -35,10 +35,6 @@ func (n *Node) PoisServiceRequestGenTag(
 	conn, err := grpc.Dial(
 		addr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithDefaultCallOptions(
-			grpc.MaxCallRecvMsgSize(128*1024*1024),
-			grpc.MaxCallSendMsgSize(128*1024*1024),
-		),
 	)
 	if err != nil {
 		return nil, err
