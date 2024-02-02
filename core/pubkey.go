0eaa90ab0e12cdb182b5116101cfc40e7683d5d9
AstaFrode <frode@cess.one>
2024-02-02 15:59:47 +0800

    add pubkey.proto

---
+++ core/pubkey.go
@@ -0,0 +1,87 @@
+/*
+	Copyright (C) CESS. All rights reserved.
+	Copyright (C) Cumulus Encrypted Storage System. All rights reserved.
+
+	SPDX-License-Identifier: Apache-2.0
+*/
+
+package core
+
+import (
+	"context"
+	"time"
+
+	"github.com/CESSProject/p2p-go/pb"
+	"google.golang.org/grpc"
+)
+
+func (n *Node) NewPubkeyApiClient(addr string, opts ...grpc.DialOption) (pb.CesealPubkeysProviderClient, error) {
+	conn, err := grpc.Dial(addr, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return pb.NewCesealPubkeysProviderClient(conn), nil
+}
+
+func (n *Node) GetIdentityPubkey(
+	addr string,
+	request *pb.Request,
+	timeout time.Duration,
+	dialOpts []grpc.DialOption,
+	callOpts []grpc.CallOption,
+) (*pb.IdentityPubkeyResponse, error) {
+	conn, err := grpc.Dial(addr, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	c := pb.NewCesealPubkeysProviderClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	result, err := c.GetIdentityPubkey(ctx, request, callOpts...)
+	return result, err
+}
+
+func (n *Node) GetMasterPubkey(
+	addr string,
+	request *pb.Request,
+	timeout time.Duration,
+	dialOpts []grpc.DialOption,
+	callOpts []grpc.CallOption,
+) (*pb.MasterPubkeyResponse, error) {
+	conn, err := grpc.Dial(addr, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	c := pb.NewCesealPubkeysProviderClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	result, err := c.GetMasterPubkey(ctx, request, callOpts...)
+	return result, err
+}
+
+func (n *Node) GetPodr2Pubkey(
+	addr string,
+	request *pb.Request,
+	timeout time.Duration,
+	dialOpts []grpc.DialOption,
+	callOpts []grpc.CallOption,
+) (*pb.Podr2PubkeyResponse, error) {
+	conn, err := grpc.Dial(addr, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	c := pb.NewCesealPubkeysProviderClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	result, err := c.GetPodr2Pubkey(ctx, request, callOpts...)
+	return result, err
+}
