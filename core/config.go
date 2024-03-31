398c61f56713e2a809d3c5380a83d998b64708c0
AstaFrode <frode@cess.one>
2024-03-31 23:27:24 +0800

    update dir mode

--- core/config.go
+++ core/config.go
@@ -21,7 +21,7 @@ const (
 
 	ZeroFileHash_16M = "080acf35a507ac9849cfcba47dc2ad83e01b75663a516279c8b9d243b719643e"
 
-	DirMode = 0644
+	DirMode = 0755
 
 	SIZE_1KiB = 1024
 	SIZE_1MiB = 1024 * SIZE_1KiB
